edff1fe0e84690f747587a91a8378bb50132bc45
Boris Kamtou <boriskamtou@gmail.com>
2026-08-20 23:46:59 +0100

    Setup configs

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module expense-tracker
 
 go 1.26
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.35.1 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"expense-tracker/utils"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+	JWT      JWTConfig
+}
+type ServerConfig struct {
+	Port    string
+	GinMode string
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+type JWTConfig struct {
+	Secret                string
+	JwtExpiresIn          time.Duration
+	RefreshTokenExpiresIn time.Duration
+}
+
+func LoadConfig() (*Config, error) {
+	_ = godotenv.Load()
+
+	jwtExpiresIn, _ := time.ParseDuration(utils.GetEnv("JWT_EXPIRES_IN", "15m"))
+	refreshTokenExpiresIn, _ := time.ParseDuration(utils.GetEnv("REFRESH_TOKEN_EXPIRES_IN", "15m"))
+
+	config := &Config{
+		Server: ServerConfig{
+			Port:    utils.GetEnv("PORT", "8080"),
+			GinMode: utils.GetEnv("GIN_MODE", "debug"),
+		},
+		Database: DatabaseConfig{
+			Host:     utils.GetEnv("DB_HOST", "localhost"),
+			Port:     utils.GetEnv("DB_PORT", "5432"),
+			User:     utils.GetEnv("DB_USER", "postgres"),
+			Password: utils.GetEnv("DB_PASSWORD", "postgres"),
+			Name:     utils.GetEnv("DB_NAME", "ecommerce"),
+			SSLMode:  utils.GetEnv("DB_SSLMODE", "disable"),
+		},
+		JWT: JWTConfig{
+			Secret:                utils.GetEnv("JWT_SECRET", "your_jwt_secret"),
+			JwtExpiresIn:          jwtExpiresIn,
+			RefreshTokenExpiresIn: refreshTokenExpiresIn,
+		},
+	}
+
+	return config, nil
+}
